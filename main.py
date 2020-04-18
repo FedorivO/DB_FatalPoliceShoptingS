cf3a0f2b08bb32d1a7780db1f85b01c3c70583e0
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-04-18 16:58:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,51 @@
 import cx_Oracle
-
-username = 'student'
-password = 'admin'
-db = 'localhost/xe'
-
-connection = cx_Oracle.connect(username, password, db)
+username = 'user2'
+password = 'user2'
+database = 'localhost' 
+connection = cx_Oracle.connect(username,password, database)
 cursor = connection.cursor()
-
-# FIRST QUERY
-query = '''
+#query1
+print("1.Вивести місто та загальну кількість випадків вбивст під час затримання в ньому. Місто - кількість випадків.\n")
+query = """
 SELECT criminal.city_name,
     COUNT(criminal.shoot_id) as number_of_incidents
 FROM criminal
 GROUP BY criminal.city_name
-'''
+"""
+cursor.execute(query) 
 
-cursor.execute(query)
+for row in cursor:
+    print(row)
+    
 
-record = cursor.fetchone()
 
-print("FIRST QUERY")
-print("|city_name           |number_of_incidents|")
-print('|' + '-' * 20 + '|' + '-' * 19 + '|')
-while record:
-	print(f"|{record[0]:20s}|{record[1]:19d}|")
-	record = cursor.fetchone()
-
-#SECOND QUERY
-query = '''
+#query2
+print("2.Вивести місто та відсоток вбивст під час затримання відносно випадків в інших містах. Місто - відсоток від загальної кількості.\n")
+query = """
 SELECT criminal.city_name,
     ROUND(COUNT(criminal.shoot_id)*100/(SELECT COUNT(criminal.city_name) from criminal ), 1) as percent_of_incidents
 FROM criminal
 GROUP BY criminal.city_name
 ORDER BY criminal.city_name
-'''
+"""
+cursor.execute(query) 
 
-cursor.execute(query)
+for row in cursor:
+    print (row) 
 
-record = cursor.fetchone()
 
-print("\nSECOND QUERY")
-print("|city_name           |percent_of_incidents|")
-print('|' + '-' * 20 + '|' + '-' * 20 + '|')
-while record:
-	print(f"|{record[0]:20d}|{record[1]:20.1f}|")
-	record = cursor.fetchone()
-
-#THIRD QUERY
-query = '''
-SELECT YEAR(date_),
-    COUNT(criminal.city_name)
+#query3
+print("3.Вивести динаміку зміни кількості вбивст під час затримання залежно від дати. Рік- кількість вбивств.\n")
+query = """
+SELECT EXTRACT(YEAR FROM criminal.date_),COUNT(criminal.city_name)
 FROM criminal
-GROUP BY YEAR(date_)
-ORDER BY YEAR(date_)
-'''
+GROUP BY  EXTRACT(YEAR FROM criminal.date_)
 
-cursor.execute(query)
+ORDER BY EXTRACT(YEAR FROM criminal.date_)
+"""
+cursor.execute(query) 
 
-record = cursor.fetchone()
-
-print("\nTHIRD QUERY")
-print("|year |incident_num")
-print('|' + '-' * 5 + '|' + '-' * 13 + '|')
-while record:
-	print(f"|{record[0]:5d}|{record[1]:13d}|")
-	record = cursor.fetchone()
-
-
+for row in cursor:
+    print (row) 
 cursor.close()
 connection.close()
