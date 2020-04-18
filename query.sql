56944694ee5a4bbbc90801983356d2e6f7efd95c
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-04-18 12:40:31 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,12 @@
 
 --QUERY 1
 
--- Вивести місто та загальну кількість випадків вбивст під час затримання в ньому.
---Місто - кількість випадків.
-
 SELECT criminal.city_name,
     COUNT(criminal.shoot_id) as number_of_incidents
 FROM criminal
 GROUP BY criminal.city_name;
 
 --QUERY 2
-
----Вивести місто та відсоток вбивст під час затримання відносно випадків в інших містах.
---Місто - відсоток від загальної кількості
-
 
 
 SELECT criminal.city_name,
@@ -23,7 +16,6 @@ GROUP BY criminal.city_name
 ORDER BY criminal.city_name;
 
 --QUERY 3
--- Вивести динаміку зміни кількості вбивст під час затримання залежно від дати.
 
 SELECT YEAR(date_),
     COUNT(criminal.city_name)
