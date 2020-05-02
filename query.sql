373df6197e941b91351d450e4c0c723926bdb90b
FedorivO <44712938+FedorivO@users.noreply.github.com>
2020-05-02 14:46:46 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,23 +1,23 @@
 
 --QUERY 1
 
-SELECT criminal.city_name,
+SELECT city_name,
     COUNT(criminal.shoot_id) as number_of_incidents
 FROM criminal
-GROUP BY criminal.city_name;
+GROUP BY city_name;
 
 --QUERY 2
 
 
-SELECT criminal.city_name,
-    ROUND(COUNT(criminal.shoot_id)*100/(SELECT COUNT(criminal.city_name) from criminal ), 1) as percent_of_incidents
+SELECT city_name,
+    ROUND(COUNT(criminal.shoot_id)*100/(SELECT COUNT(city_name) from criminal ), 1) as percent_of_incidents
 FROM criminal
-GROUP BY criminal.city_name
-ORDER BY criminal.city_name;
+GROUP BY city_name
+ORDER BY city_name;
 
 --QUERY 3
 
-SELECT EXTRACT(YEAR FROM criminal.date_),COUNT(criminal.city_name)
+SELECT EXTRACT(YEAR FROM criminal.date_),COUNT(city_name)
 FROM criminal
 GROUP BY  EXTRACT(YEAR FROM criminal.date_)
 
